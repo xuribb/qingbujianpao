ac087e605041390cdc71cac3a0b365d9e65a398d
xuribinbin <xuribinbin@foxmail.com>
2025-08-22 19:26:03 +0800

    登录页

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,15 @@
 Page({
-  data: {
-
-  }
+    data: {
+        latitude: 0,
+        longitude: 0
+    },
+    onLoad() {
+        wx.getLocation({
+            type: "gcj02",
+            success(res) {
+                this.latitude = res.latitude;
+                this.longitude = res.longitude;
+            }
+        });
+    }
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,75 +1,86 @@
 Page({
-  data: {
-    motto: '心中有信仰 前路不迷茫',
-    userInfo: {
-      avatarUrl: "",
-      nickName: "",
+    data: {
+        motto: '心中有信仰 前路不迷茫',
+        userInfo: {
+            avatarUrl: "",
+            nickName: "",
+        },
     },
-  },
-  onLoad() {
-    const app = getApp();
-    this.setData({
-      "userInfo.avatarUrl": app.globalData.backend + "/static/images/defaultAvatar.png",
-    });
-  },
-  onChooseAvatar(e) {
-    this.setData({
-      "userInfo.avatarUrl": e.detail.avatarUrl
-    });
-  },
-  onInputChange(e) {
-    const nickName = e.detail.value;
-    this.setData({
-      "userInfo.nickName": nickName
-    });
-  },
-  onRegister(e) {
-    if (!this.data.userInfo.nickName) {
-      return wx.showToast({
-        title: '昵称不能为空',
-        icon: 'error',
-      });
-    }
-
-    wx.login({
-      success: res => {
+    onLoad() {
+        // const app = getApp();
+        // this.setData({
+        //     "userInfo.avatarUrl": app.globalData.backend + "/static/images/defaultAvatar.png",
+        // });
+    },
+    toLogin() {
         const app = getApp();
-        const backend = app.globalData.backend;
-        wx.request({
-          url: `${backend}/Login2Register/register`,
-          method: 'POST',
-          data: {
-            code: res.code,
-            nickName: this.data.userInfo.nickName,
-            avatarUrl: this.data.userInfo.avatarUrl
-          },
-          success: (res) => {
-            if (res.data.code) {
-              wx.showToast({
-                title: res.data.errmsg,
-                icon: 'error'
-              });
-            } else {
-              wx.setStorage({
-                key: 'userInfo',
-                data: res.data.data,
-                success: () => {
-                  app.globalData.userInfo = res.data.data;
-                  wx.switchTab({
-                    url: '/pages/me/me',
-                  })
+        const domain = app.globalData.domain;
+
+        wx.login({
+            success(res) {
+                if (res.code) {
+                    wx.request({
+                        url: domain + '/user/code2Session',
+                        data: {
+                            code: res.code
+                        },
+                        success(res) {
+                            console.log(res);
+                        }
+                    });
+                } else {
+                    wx.showToast({
+                        title: '登录失败：' + res.errMsg,
+                        icon: 'error'
+                    });
                 }
-              });
             }
-          }
-        })
-      },
-      fail: err => {
-        wx.showToast({
-          title: '获取用户CODE失败，稍后重试',
-          icon: 'error'
         });
-      }
-    })
-  }
+        // wx.getUserProfile({
+        //     desc: '获取用户昵称和头像',
+        //     success: res => {
+        //         const app = getApp();
+        //         app.globalData.userInfo.avatarUrl = res.userInfo.avatarUrl;
+        //         app.globalData.userInfo.nickName = res.userInfo.nickName;
+
+        //         return console.log(res);
+        //         // const app = getApp();
+        //         // const backend = app.globalData.backend;
+        //         // wx.request({
+        //         //     url: `${backend}/Login2Register/register`,
+        //         //     method: 'POST',
+        //         //     data: {
+        //         //         code: res.code,
+        //         //         nickName: this.data.userInfo.nickName,
+        //         //         avatarUrl: this.data.userInfo.avatarUrl
+        //         //     },
+        //         //     success: (res) => {
+        //         //         if (res.data.code) {
+        //         //             wx.showToast({
+        //         //                 title: res.data.errmsg,
+        //         //                 icon: 'error'
+        //         //             });
+        //         //         } else {
+        //         //             wx.setStorage({
+        //         //                 key: 'userInfo',
+        //         //                 data: res.data.data,
+        //         //                 success: () => {
+        //         //                     app.globalData.userInfo = res.data.data;
+        //         //                     wx.switchTab({
+        //         //                         url: '/pages/me/me',
+        //         //                     })
+        //         //                 }
+        //         //             });
+        //         //         }
+        //         //     }
+        //         // })
+        //     },
+        //     fail: err => {
+        //         wx.showToast({
+        //             title: '获取用户CODE失败，稍后重试',
+        //             icon: 'error'
+        //         });
+        //     }
+        // });
+    }
 })
