e0713b02d6f471a8a4e5fbe27bf15280bbeb46a2
xuribinbin <xuribinbin@foxmail.com>
2025-08-23 19:28:31 +0800

    天气题图

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 App({
     onLaunch() {
-        // const userInfo = wx.getStorageSync('userInfo');
-        // if (userInfo) {
-        //     this.globalData.userInfo = userInfo;
-        //     return wx.switchTab({
-        //         url: '/pages/me/me',
-        //     });
-        // }
+        const openid = wx.getStorageSync('openid');
+        if (openid) {
+            this.globalData.openid = openid;
+        } else {
+            const domain = this.globalData.domain;
+            wx.login({
+                success: res => {
+                    if (res.code) {
+                        wx.request({
+                            url: domain + '/user/code2Session',
+                            data: {
+                                code: res.code
+                            },
+                            success: res => {
+                                if (res.data.code) {
+                                    wx.setStorage({
+                                        key: "openid",
+                                        data: res.data.data.openid,
+                                    });
+                                    this.globalData.openid = res.data.data.openid;
+                                } else {
+                                    wx.showToast({
+                                        title: res.data.msg,
+                                        icon: 'error'
+                                    });
+                                }
+                            }
+                        });
+                    } else {
+                        wx.showToast({
+                            title: '登录失败：' + res.errMsg,
+                            icon: 'error'
+                        });
+                    }
+                }
+            });
+        }
     },
     globalData: {
         domain: "http://qingbujianpao.learn",
-        userInfo: {
-            avatarUrl: "",
-            nickName: ""
-        }
+        openid: "",
     }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,15 +1,17 @@
 Page({
-    data: {
-        latitude: 0,
-        longitude: 0
-    },
-    onLoad() {
-        wx.getLocation({
-            type: "gcj02",
-            success(res) {
-                this.latitude = res.latitude;
-                this.longitude = res.longitude;
-            }
+  data: {
+    latitude: 0,
+    longitude: 0
+  },
+  onLoad() {
+    wx.getLocation({
+      type: "gcj02",
+      success: res => {
+        this.setData({
+          latitude: res.latitude,
+          longitude: res.longitude
         });
-    }
+      }
+    });
+  }
 })
