206ab540cfe3f57f1f6b256f1981a0243cabc69a
xuribinbin <xuribinbin@foxmail.com>
2025-08-26 19:24:27 +0800

    跑步

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
 App({
-    onLaunch() {
-        const openid = wx.getStorageSync('openid');
-        if (openid) {
-            this.globalData.openid = openid;
-        } else {
-            const domain = this.globalData.domain;
-            wx.login({
-                success: res => {
-                    if (res.code) {
-                        wx.request({
-                            url: domain + '/user/code2Session',
-                            data: {
-                                code: res.code
-                            },
-                            success: res => {
-                                if (res.data.code) {
-                                    wx.setStorage({
-                                        key: "openid",
-                                        data: res.data.data.openid,
-                                    });
-                                    this.globalData.openid = res.data.data.openid;
-                                } else {
-                                    wx.showToast({
-                                        title: res.data.msg,
-                                        icon: 'error'
-                                    });
-                                }
-                            }
-                        });
-                    } else {
-                        wx.showToast({
-                            title: '登录失败：' + res.errMsg,
-                            icon: 'error'
-                        });
-                    }
+  onLaunch() {
+    this.getOpenID();
+  },
+  getOpenID() {
+    const openid = wx.getStorageSync('openid');
+    if (openid) {
+      this.globalData.openid = openid;
+    } else {
+      const domain = this.globalData.domain;
+      wx.login({
+        success: res => {
+          if (res.code) {
+            wx.request({
+              url: domain + '/user/code2Session',
+              data: {
+                code: res.code
+              },
+              success: res => {
+                if (res.data.code) {
+                  wx.setStorage({
+                    key: "openid",
+                    data: res.data.data.openid,
+                  });
+                  this.globalData.openid = res.data.data.openid;
+                } else {
+                  wx.showToast({
+                    title: res.data.msg,
+                    icon: 'error'
+                  });
                 }
+              }
             });
+          } else {
+            wx.showToast({
+              title: '登录失败：' + res.errMsg,
+              icon: 'error'
+            });
+          }
         }
-    },
-    globalData: {
-        domain: "http://qingbujianpao.learn",
-        openid: "",
+      });
     }
+  },
+  globalData: {
+    domain: "http://qingbujianpao.learn",
+    openid: "",
+  }
 })
--- pages/run/run.js
+++ pages/run/run.js
@@ -1,14 +1,52 @@
 Page({
   data: {
-    latitude: 0, 
-    longitude: 0
+    latitude: 27.776341,
+    longitude: 104.07254,
+    polyline: [{
+      points: [{
+          latitude: 27.776341,
+          longitude: 104.07254
+        },
+        {
+          latitude: 27.775341,
+          longitude: 104.07254
+        },
+        {
+          latitude: 27.774341,
+          longitude: 104.07254
+        },
+        {
+          latitude: 26.774341,
+          longitude: 104.07254
+        },
+        {
+          latitude: 27.774341,
+          longitude: 105.07254
+        },
+        {
+          latitude: 25.774341,
+          longitude: 104.07254
+        },
+      ],
+      color: '#58c16c',
+      width: 6,
+      borderColor: '#2f693c',
+      borderWidth: 1
+    }]
   },
   onLoad() {
-    wx.startLocationUpdateBackground({
-      complete: res => {
-        console.log(res);
-      }
-    });
-  },
-  locationChange(res) {}
+    // wx.onLocationChange(res => {
+    //   this.setData({
+    //     latitude: res.latitude,
+    //     longitude: res.longitude
+    //   });
+    //   console.log('location change', res);
+    // });
+
+    // wx.startLocationUpdateBackground({
+    //   complete: res => {
+    //     console.log(res);
+    //   }
+    // });
+  }
 })
